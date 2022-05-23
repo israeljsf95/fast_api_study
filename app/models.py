481da6fc29107a2eddabc29f9d0cb352ba1e8b39
Israel Filho <israelfilhocpd@gmail.com>
2022-05-23 14:23:49 -0300

    models_users

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key = True, nullable = False)
-    email = Column(String, nullable = False, unique = True)
+    email = Column(String, nullable = False, unique = True)image.png
     password = Column(String, nullable = False)
     created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text("now()"))
     
